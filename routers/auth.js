d4ebf732f357427a378e47d1d03d6803d5a9d9fc
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-10-13 15:31:02 +0300

    the register route added and saved to db

--- routers/auth.js
+++ routers/auth.js
@@ -1,13 +1,20 @@
-const router = require('express').Router();
-const User = require('../models/User');
+const router = require('express').Router()
+const User = require('../models/User')
 
+router.post('/register', async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  })
 
-router.get('/register', (req, res) => {
+  try {
+    const savedUser = await user.save()
+    res.send(savedUser)
+  } catch (err) {
+    console.log(err)
+    res.status(400).send(err)
+  }
+})
 
-
-
-});
-
-
-
-module.exports = router;
+module.exports = router
