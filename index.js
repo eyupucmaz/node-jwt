2baacf70b443831668e9efdc1917d36374c8fe3b
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-10-13 15:30:11 +0300

    Fix db connection and change prettier conf

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+// Import routes
+const authRouter = require('./routers/auth.js')
 
 // Config dotenv
-dotenv.config();
+dotenv.config()
+
+// Connect to DB
+mongoose.connect(process.env.CONNECT_DB, { useNewUrlParser: true }, () => {
+  console.log('connected to db')
+})
+
+// Route middlewares
+app.use(express.json())
+app.use('/api/auth', authRouter)
 
 // Start server
 app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server started on port: ${process.env.SERVER_PORT}`);
-});
-
-// Connect to DB
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-  console.log('Connected to DB!');
-});
-
-
-// Import routes
-const authRouter = require('./routers/auth.js');
-
-// Route middlewares
-app.use(express.json());
-app.use('/api/auth', authRouter);
+  console.log(`Server started on port: ${process.env.SERVER_PORT}`)
+})
